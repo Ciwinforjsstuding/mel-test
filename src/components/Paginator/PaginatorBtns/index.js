92726f54984b8e56fcdc7e1f896c29df66be43e6
Ciwinforjsstuding <spid2001@yandex.ru>
2021-10-22 22:26:53 +0300

    fix bug with paginator btns in favorit list

--- src/components/Paginator/PaginatorBtns/index.js
+++ src/components/Paginator/PaginatorBtns/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { FAVORIT } from '../../../store/constants';
 
 import { changePagePaginator } from '../../../store/StarWars/starWarsSlice';
 import Button from '../../../UI/Button';
@@ -10,24 +11,31 @@ const PaginatorBtns = ({
   pages,
   currentPage,
   changePagePaginator,
-}) => (
-  <div className="paginator-btns flex justify-center items-center">
-    {pages.map(page => (
-      <Button
-        key={page}
-        clickHandler={() => changePagePaginator(page)}
-        customCssBtn={`paginator__btn ${
-          currentPage === page && 'current-page'
-        }`}>
-        {page}
-      </Button>
-    ))}
-  </div>
-);
+  whichShowList,
+}) => {
+  if (whichShowList === FAVORIT) {
+    return null;
+  }
+  return (
+    <div className="paginator-btns flex justify-center items-center">
+      {pages.map(page => (
+        <Button
+          key={page}
+          clickHandler={() => changePagePaginator(page)}
+          customCssBtn={`paginator__btn ${
+            currentPage === page && 'current-page'
+          }`}>
+          {page}
+        </Button>
+      ))}
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   pages: state.starWars.pages,
   currentPage: state.starWars.currentPage,
+  whichShowList: state.starWars.whichShowList,
 });
 
 const mapDispatchToProps = {
